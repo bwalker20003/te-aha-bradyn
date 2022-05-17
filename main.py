96f45744693c98e8fc25a9d61866b45b1f02ed63
Bradyn Walker <>
2022-05-17 01:15:16 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,43 @@
+#13/5 bradyn 1 question:say hi to the user and ask them there first question
+print("kia ora user ")
+print("what is youtube in maori")
+answer = input ()
+answer = answer .lower()
+#13/5 bradyn answer: make it were if they say yes or no it has a respond
+if answer == "tiriata": 
+ print("you are correct")
+else:
+  print(" sorry you are wrong")
+#ask them there seacond question 
+print("what is gaming in maori")
+answer = input ()
+answer = answer .lower()
+if answer == "petipeti": 
+ print("you are correct")
+else:
+  print(" sorry you are wrong")
+
+print("what is twitter in maori")
+answer = input ()
+answer = answer .lower()
+if answer == "tihau": 
+ print("you are correct")
+else:
+  print(" sorry you are wrong")
+
+print("what is battery in maori")
+answer = input ()
+answer = answer .lower()
+if answer == "puhiko": 
+ print("you are correct")
+else:
+  print(" sorry you are wrong")
+
+print("what is wifi in maori")
+answer = input ()
+answer = answer .lower()
+if answer == "ahorore": 
+ print("you are correct")
+else:
+  print(" sorry you are wrong")
+print("kia ora user ")
