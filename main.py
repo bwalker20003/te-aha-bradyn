ef6fc2e026949f6759cd42545fd5c40a0b83972e
Bradyn Walker <101752909+bwalker20003@users.noreply.github.com>
2022-05-18 03:00:22 +0000

    I changed the start to ask what the users  name is and then to hi

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-#13/5 bradyn 1 question:say hi to the user and ask them there first question
-print("kia ora user ")
-print("what is youtube in maori")
+#13/5 bradyn first question:say hi to the user and ask them there first question
+print("what is your name")
+n = input ()
+print("kia ora " + n )
+print("what is youtube in maaori")
 answer = input ()
 answer = answer .lower()
 #13/5 bradyn answer: make it were if they say yes or no it has a respond
@@ -8,36 +10,37 @@ if answer == "tiriata":
  print("you are correct")
 else:
   print(" sorry you are wrong")
-#ask them there seacond question 
-print("what is gaming in maori")
+#18/5 bradyn:ask them there seacond question 
+print("what is gaming in maaori")
 answer = input ()
 answer = answer .lower()
 if answer == "petipeti": 
  print("you are correct")
 else:
   print(" sorry you are wrong")
-
-print("what is twitter in maori")
+#18/5 bradyn:ask them there third question
+print("what is twitter in maaori")
 answer = input ()
 answer = answer .lower()
 if answer == "tihau": 
  print("you are correct")
 else:
   print(" sorry you are wrong")
-
-print("what is battery in maori")
+#18/5 bradyn:ask them there fourth question
+print("what is battery in maaori")
 answer = input ()
 answer = answer .lower()
 if answer == "puhiko": 
  print("you are correct")
 else:
   print(" sorry you are wrong")
-
-print("what is wifi in maori")
+#18/5 bradyn:ask them there fifth question
+print("what is wifi in maaori")
 answer = input ()
 answer = answer .lower()
 if answer == "ahorore": 
  print("you are correct")
 else:
   print(" sorry you are wrong")
-print("kia ora user ")
+
+print("kia ora " + n )
