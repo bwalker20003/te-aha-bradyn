86803bf9cc6c70824e2acd45857e901e190c29dc
Bradyn Walker <101752909+bwalker20003@users.noreply.github.com>
2022-06-13 00:54:21 +0000

    today I tested my code and found out that you need to have spaces for the last question

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
- #13/5 bradyn first question:say hi to the user and ask them there first question
+ #13/5 bradyn 21 first question:say hi to the user and ask them there first question
 print("what is your name")
 n = input ()
 print("kia ora " + n )
@@ -6,7 +6,7 @@ print("kia ora " + n )
 print("what is hello in maaori")
 answer = input ()
 answer = answer .lower()
-#13/5 bradyn answer: make it were if they say yes or no it has a respond
+#13/5 bradyn 22 answer: make it were if they say yes or no it has a respond
 if answer == "kia ora":
   print("you are correct")
 
@@ -19,7 +19,7 @@ elif answer == "tena kōrua":
 else:
   print("you are wrong")
  
-#18/5 bradyn:ask them there seacond question 
+#18/5 bradyn 23:ask them there seacond question 
 print("what is bye in maaori")
 answer = input ()
 answer = answer .lower()
@@ -31,7 +31,7 @@ elif answer == "haere ra":
   print("you are correct")
 else:
   print(" sorry you are wrong")
-#18/5 bradyn:ask them there third question
+#18/5 bradyn 24:ask them there third question
 print("what is family in maaori")
 answer = input ()
 answer = answer .lower()
@@ -39,23 +39,28 @@ if answer == "whanau":
  print("you are correct")
 else:
   print(" sorry you are wrong")
-#18/5 bradyn:ask them there fourth question
+#18/5 bradyn 25:ask them there fourth question
 print("what is friend in maaori")
 answer = input ()
 answer = answer .lower()
 if answer == "hoa": 
  print("you are correct")
+elif answer == "tai":
+  print("you are correct")
+elif answer == "whakahoahoa":
+  print("you are correct")
 else:
   print(" sorry you are wrong")
-#18/5 bradyn:ask them there fifth question
+#18/5 bradyn 26:ask them there fifth question
 print("what is fraser in maaori")
 answer = input ()
 answer = answer .lower()
 if answer == "taniwharau": 
  print("you are correct")
+
 else:
   print(" sorry you are wrong")
-#i am making more question to my quiz
+# 28/6 bradyn 27: i am making more question to my quiz
 print("what is high school  in maaori")
 answer = input ()
 answer = answer .lower()
@@ -66,10 +71,30 @@ else:
 print("what is fraser high school  in maaori")
 answer = input ()
 answer = answer .lower()
-#it keeps saying that it is wrong
+#28/6 bradyn 28:it keeps saying that it is wrong
 if answer == "te kura tuarua o taniwharau": 
  print("you are correct")
 else:
   print("you are wrong")
 
 print("kia ora " + n )
+
+print("do you wont to know some thing")
+answer = input ()
+answer = answer .lower()
+if answer == "yes": 
+ print("you are not cool")
+elif answer == "of course":
+ print("you can be the one")
+ answer_option = input("do you wont to be the one? ")
+ if answer_option == "yes": 
+   print("ok you will have to kill people")
+ elif answer_option == "of course":
+   print("ok here is $100,000,000,000")
+ else:
+   print("ok then you have to die")
+else:
+  print("ok then good bye and have a good day")
+
+
+  
